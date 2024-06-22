c39665ed90dd97b74c9bf8e970224bde12edb4c8
taewoong <rlaxodnd95@gmail.com>
2024-06-22 19:22:28 +0900

    :zap: 스플래시 페이지 추가, 주간 달력 1차 완성

---
+++ src/pages/home.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Calendar from '@/components/Calendar.vue';
+import Today from '@/components/Today.vue';
+
+const showType = ref('today');
+</script>
+<template>
+  <main class="flex flex-col gap-[20px]">
+    <div class="overflow-x-auto flex items-center shrink-0 justify-center gap-[10px]">
+      <button
+        class="px-[4px] py-[2px] border rounded-md text-xs shadow-sm cursor-pointer"
+        :class="
+          showType === 'today'
+            ? 'bg-accent text-white border-accent'
+            : 'border-gray-300 hover:bg-gray-50'
+        "
+        @click="showType = 'today'"
+      >
+        오늘 경기
+      </button>
+      <button
+        class="px-[4px] py-[2px] border rounded-md text-xs shadow-sm cursor-pointer"
+        :class="
+          showType === 'calendar'
+            ? 'bg-accent text-white border-accent'
+            : 'border-gray-300 hover:bg-gray-50'
+        "
+        @click="showType = 'calendar'"
+      >
+        일정 달력
+      </button>
+    </div>
+    <today v-if="showType === 'today'" />
+    <calendar v-else />
+  </main>
+</template>
